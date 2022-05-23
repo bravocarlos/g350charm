604344d2c7cccd2aa0ce7015f38cca7938d90c88
Carlos Bravo <carlos@carlosbravotoledo.com>
2022-05-23 10:08:36 -0400

    G350 Initial push

---
+++ README.md
@@ -0,0 +1,19 @@
+g350 Storage Backend for Cinder
+-------------------------------
+
+Overview
+========
+
+This charm provides a g350 storage backend for use with the Cinder
+charm.
+
+To use:
+
+    juju deploy cinder
+    juju deploy cinder-g350
+    juju add-relation cinder-g350 cinder
+
+Configuration
+=============
+
+See config.yaml for details of configuration options.
---
+++ src/charm.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+
+# Copyright 2021 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import logging
+
+from ops.main import main
+from ops_openstack.plugins.classes import CinderStoragePluginCharm
+
+logger = logging.getLogger(__name__)
+
+
+class Cinderg350Charm(CinderStoragePluginCharm):
+
+    PACKAGES = [
+        'cinder-common']
+
+    stateless = False
+    active_active = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._stored.is_started = True
+
+    def cinder_configuration(self, charm_config) -> 'list[tuple]':
+        """Return the configuration to be set by the principal"""
+        cget = charm_config.get
+
+        volume_backend_name = cget(
+            'volume-backend-name') or self.framework.model.app.name
+
+        
+
+        raw_options = [
+            ('volume_backend_name', volume_backend_name),
+            ('hitachi_pool', cget('hitachi_pool')),
+        ]
+
+        options = [(x, y) for x, y in raw_options if y]
+        return options
+
+
+if __name__ == '__main__':
+    main(Cinderg350Charm)
